819b153500dc9a1bfb7e776c083e0f8c14d9bd58
SnapDragonGR <thesanegleb@gmail.com>
2026-09-16 02:05:43 -0400

    practice data streams and update the mega update script to log results and errors

--- bash-scripting/update-script-upgraded.sh
+++ bash-scripting/update-script-upgraded.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if [ -f "$release_file" ] && [ -r "$release_file" ]
 then
     if grep -q "Arch" "$release_file"; then
-        sudo pacman -Syu
+        sudo pacman -Syu &>>"$logfile"
     elif grep -q "Ubuntu" "$release_file" || grep -q "Debian" "$release_file"; then
-        sudo apt update && sudo apt dist-upgrade
+        { sudo apt update && sudo apt dist-upgrade -y; } &>>"$logfile"
     elif grep -q "Fedora" "$release_file" || grep -q "rhel" "$release_file"; then
-        sudo dnf upgrade -y
+        sudo dnf upgrade -y &>>"$logfile"
     else
         echo "Unsupported operating system."
         exit 2
