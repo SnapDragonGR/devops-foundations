193763bd92877c9d0cd143f049213e083bf88c43
SnapDragonGR <thesanegleb@gmail.com>
2026-09-17 09:59:14 -0400

    modulize the script even more - add run_update func

--- bash-scripting/update-script-upgraded.sh
+++ bash-scripting/update-script-upgraded.sh
@@ -2,30 +2,29 @@
 
 release_file=/etc/os-release
 logfile=/var/log/updater.log
-errorlog=/var/log/updater_errors.log
 
-check_exit_status() {
-    if [ $? -ne 0 ]
-    then
-        echo "An error occurred, please check the $errorlog file."
-        exit 1
+run_update() {
+    if "$@" &>>"$logfile"; then
+        echo "Update was successful."
+        return 0
+    else
+        echo "Update was not successful - check $logfile for logs."
+        return 1
     fi
 }
 
 if [ -f "$release_file" ] && [ -r "$release_file" ]
 then
     if grep -q "Arch" "$release_file"; then
-        sudo pacman -Syu &>>"$logfile"
+        run_update sudo pacman -Syu
     elif grep -q "Ubuntu" "$release_file" || grep -q "Debian" "$release_file"; then
-        { sudo apt update && sudo apt dist-upgrade -y; } &>>"$logfile"
+        run_update sudo apt update && run_update sudo apt dist-upgrade
     elif grep -q "Fedora" "$release_file" || grep -q "rhel" "$release_file"; then
-        sudo dnf upgrade -y &>>"$logfile"
+        run_update sudo dnf upgrade
     else
         echo "Unsupported operating system."
         exit 2
     fi
-    check_exit_status
-    echo "Update was successful. Enjoy"
 else
     echo "os-release file not found."
     exit 1
